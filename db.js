ccb7c0c4caa0bde926f463c2117e4d74851ac979
sanaru <sanaru9@gmail.com>
2018-10-10 22:39:17 +0530

    db file corrected

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	DB: 'mongodb://ds227243.mlab.com:27243/heroku_zpvxvpp2'
+	DB: 'mongodb://react_node_file_upload:sana290594@ds227243.mlab.com:27243/heroku_zpvxvpp2'
     // DB: process.env.MONGODB_URI
 }
